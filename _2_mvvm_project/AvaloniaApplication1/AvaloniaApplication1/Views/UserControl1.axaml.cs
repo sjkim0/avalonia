871d8368b118e0f9ec0ad5624be9b2b8970f6580
ksj10111011@naver.com <ksj10111011@naver.com>
2024-09-29 00:15:49 +0900

    user control 동작 x ... avalonia example 쭉 따라하며 진행할예정

--- _2_mvvm_project/AvaloniaApplication1/AvaloniaApplication1/Views/UserControl1.axaml.cs
+++ _2_mvvm_project/AvaloniaApplication1/AvaloniaApplication1/Views/UserControl1.axaml.cs
@@ -7,20 +7,29 @@ namespace AvaloniaApplication1;
 
 public partial class UserControl1 : UserControl
 {
-    public static readonly DirectProperty<UserControl1, string> MyTextProperty =
-        AvaloniaProperty.RegisterDirect<UserControl1, string>(
-            nameof(MyText),
-            o => o.MyText,
-            (o, v) => o.MyText = v);
+    public static readonly DirectProperty<UserControl1, int> ValueProperty =
+    AvaloniaProperty.RegisterDirect<UserControl1, int>(
+        nameof(Value),                            // The name of the property     
+        o => o.Value,                  // The getter of the property
+        (o, v) => o.Value = v,      // The setter of the property
+        defaultBindingMode: BindingMode.TwoWay,   // We change the default binding mode to be two-way, so if the user selects a new value, it will automatically update the bound property
+        enableDataValidation: true);              // Enables DataValidation
 
-    private string _myText;
-    public string MyText
+    // For direct properties we need to have a backing field
+    private int _value;
+
+    /// <summary>
+    /// Gets or sets the current value
+    /// </summary>
+    public int Value
     {
-        get => _myText;
-        set => SetAndRaise(MyTextProperty, ref _myText, value);
+        get { return _value; }
+        set { SetAndRaise(ValueProperty, ref _value, value); }
     }
+
     public UserControl1()
     {
         InitializeComponent();
+        DataContext = this;
     }
 }
